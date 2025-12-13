cac50a6623c2ca71aa29f65af93da04ed6d33e37
dkrivaa <dannykrivaa@hotmail.com>
2025-12-13 19:26:54 +0200

    Initial commit

--- ui/views/home.py
+++ ui/views/home.py
@@ -4,3 +4,7 @@ import streamlit as st
 def render():
     st.title("Welcome to the Home Page")
     st.write("This is the main landing page of the application.")
+
+
+if __name__ == "__main__":
+    render()
