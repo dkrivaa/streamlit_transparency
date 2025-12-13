1949d02fbdc02441d25d99206a518b593de5ce30
dkrivaa <dannykrivaa@hotmail.com>
2025-12-13 21:41:35 +0200

    Initial commit

--- ui/views/home.py
+++ ui/views/home.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import asyncio
+
+
+async def my_async():
+    return asyncio.run(asyncio.sleep(1, result="Async Result"))
 
 
 def render():
     st.title("Welcome to the Home Page")
     st.write("This is the main landing page of the application.")
+    asyncio.run(my_async())
 
 
 if __name__ == "__main__":
