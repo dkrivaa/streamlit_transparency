f43b02bc50a0b8d36a81e4b711e90c71d0e1d1da
dkrivaa <dannykrivaa@hotmail.com>
2025-12-14 10:32:15 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import subprocess
-subprocess.run(["playwright", "install", "chromium", "--with-deps"], check=True)
+subprocess.run(["playwright", "install", "chromium"], check=True)
 
 import streamlit as st
 
