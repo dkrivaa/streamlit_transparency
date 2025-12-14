4148393b5c0524938be1e9db30c7e9b88e72d58e
dkrivaa <dannykrivaa@hotmail.com>
2025-12-14 08:41:33 +0200

    Initial commit

--- ui/views/home.py
+++ ui/views/home.py
@@ -3,6 +3,8 @@ import asyncio
 
 from backend.utilities.general import run_async
 from backend.data.super_class import SupermarketChain
+from backend.data.shufersal import Shufersal
+from backend.utilities.general import get_chain_class_by_alias
 
 
 async def my_async():
@@ -13,8 +15,9 @@ async def my_async():
 def render():
     st.title("Welcome to the Home Page")
     st.write("This is the main landing page of the application.")
-    # result = run_async(my_async())
-    st.write(SupermarketChain.registry)
+    chain_alias = 'shufersal'
+    chain = run_async(get_chain_class_by_alias(chain_alias))
+    st.write(f"Loaded chain: {chain.name} ({chain.alias})")
 
 
 if __name__ == "__main__":
