a241fff5086ac00f7c87f9e8dbb4cabe933955ea
dkrivaa <dannykrivaa@hotmail.com>
2025-12-16 07:23:33 +0200

    Initial commit

--- ui/views/home.py
+++ ui/views/home.py
@@ -10,17 +10,18 @@ from backend.utilities.general import get_chain_class_by_alias
 
 def render():
     """ The main function to render the home page """
+    # Title and subtitle
     st.title("CLARITY")
     st.subheader('Supermarket Price Transparency')
     st.divider()
 
-    st.write(SupermarketChain.registry)
-
+    # Options for navigation
     option_map = {
         0: {'icon': ':material/attach_money:', 'text': 'Check product price', 'page': 'ui/views/check_price.py'},
         1: {'icon': ':material/list:', 'text': 'Compare shopping list prices', 'page': 'ui/views/shopping_list.py'},
     }
 
+    # Pills selection for navigation
     selection = st.pills(
         'Select',
         options=option_map.keys(),
@@ -29,12 +30,9 @@ def render():
         default=None,
     )
 
+    # Navigate to selected page
     if selection is not None:
         st.switch_page(option_map[selection]['page'])
-
-
-
-
 
     # chain_alias = 'ramilevi'
     # chain = get_chain_class_by_alias(chain_alias)
