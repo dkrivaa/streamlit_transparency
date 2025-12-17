6f44229edfb937a9bfd7af69b8bf1796bac1b7a5
dkrivaa <dannykrivaa@hotmail.com>
2025-12-17 17:49:14 +0200

    Initial commit

--- ui/views/db.py
+++ ui/views/db.py
@@ -7,21 +7,6 @@ from backend.utilities.general import run_async
 from backend.database.db import create_db
 
 
-async def test_asyncpg():
-    conn = await asyncpg.connect(
-        user="postgres",
-        password=st.secrets["DB_PASSWORD"],
-        host=st.secrets["DB_HOST"],   # e.g. xyz.supabase.co
-        database="postgres",
-        port=5432,
-        ssl="require"
-    )
-    val = await conn.fetchval("SELECT 1")
-    await conn.close()
-    return val
-
-
-
 def render():
     """ UI function to create the database and tables """
     st.header("Create Database")
@@ -36,13 +21,6 @@ def render():
         with st.spinner("Creating database..."):
             run_async(create_db)
         st.success("✅ Database and tables created successfully!")
-
-    if st.button("Test asyncpg"):
-        try:
-            result = run_async(test_asyncpg)
-            st.success(f"Connected! Result = {result}")
-        except Exception as e:
-            st.error(str(e))
 
 
 if __name__ == "__main__":
