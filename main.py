2bcda5ffbc61dcbe4cf9163e490f2e6a9596fbcf
dkrivaa <dannykrivaa@hotmail.com>
2025-12-17 19:43:04 +0200

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit as st
 from backend.data.super_class import SupermarketChain
 from backend.data.binaprojects import BinaProjects
 from backend.data.carrefour import Carrefour
-from backend.data.hazihinam import HaziHinam
+# from backend.data.hazihinam import HaziHinam
 from backend.data.laibcatalog import LaibCatalog
 from backend.data.publishedprices import PublishedPrices
 from backend.data.shufersal import Shufersal
