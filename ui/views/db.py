48e60e2e463e66f3dac0bd302e7c7e11d6f535ae
dkrivaa <dannykrivaa@hotmail.com>
2025-12-18 10:33:34 +0200

    Initial commit

--- ui/views/db.py
+++ ui/views/db.py
@@ -7,10 +7,13 @@ from backend.utilities.general import run_async
 from backend.database.db import create_db
 from backend.database.utilities import update_stores_db
 
+from backend.data.super_class import SupermarketChain
+
 
 def render():
     """ UI function to create the database and tables """
     st.header("Create Database")
+    st.write([cls.alias for cls in SupermarketChain.registry])
     st.write(
         """
         Click the button below to create the database and its tables.
