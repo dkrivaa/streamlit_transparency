a9b5d9fde5f56940f83e43051fb49b2339b33a13
dkrivaa <dannykrivaa@hotmail.com>
2025-12-18 16:07:55 +0200

    Initial commit

--- ui/views/check_price.py
+++ ui/views/check_price.py
@@ -42,10 +42,8 @@ def render():
             )
 
             if store:
-                if 'chain' not in st.session_state or st.session_state.chain:
-                    st.session_state.chain = chain.chain_code
-                if 'store' not in st.session_state or st.session_state.store:
-                    st.session_state.store = store
+                st.session_state.chain = chain.chain_code
+                st.session_state.store = store
 
     st.write(st.session_state)
 
