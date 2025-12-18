8c30381254105410e52dff2fbee14efd5601249c
dkrivaa <dannykrivaa@hotmail.com>
2025-12-18 17:26:32 +0200

    Initial commit

--- ui/views/check_price.py
+++ ui/views/check_price.py
@@ -46,11 +46,11 @@ def render():
                 st.session_state.alias = chain.alias
                 st.session_state.store = store
 
-                my_chain = st.session_state.get('chain', None)
-                my_store = st.session_state.get('store', None)
-                urls = run_async(my_chain.prices, store_code=my_store) if my_chain and my_store else None
-
-                st.write(urls)
+                # my_chain = st.session_state.get('chain', None)
+                # my_store = st.session_state.get('store', None)
+                # urls = run_async(my_chain.prices, store_code=my_store) if my_chain and my_store else None
+                #
+                # st.write(urls)
 
 
 if __name__ == "__main__":
