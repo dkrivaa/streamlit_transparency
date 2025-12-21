1f3d4e0e8c69f058d825c1accbe47215d8f137e7
dkrivaa <dannykrivaa@hotmail.com>
2025-12-21 13:30:03 +0200

    Initial commit

--- ui/views/check_price_2.py
+++ ui/views/check_price_2.py
@@ -80,8 +80,8 @@ def promo_element(promo: dict):
 
 def render_quantity_discount(chain: SupermarketChain, promo: dict):
     """ Renders a single promo element with reward type 1"""
-    text = rtl(promo.get('PromotionDescription', 'N/A'))
-    st.markdown(f"**{text}**")
+    # text = rtl(promo.get('PromotionDescription', 'N/A'))
+    st.markdown(f"**{promo.get('PromotionDescription', 'N/A')}**")
     st.metric(
         label="Promotion Price",
         value=f"{promo.get('DiscountedPrice')} NIS",
