b497d4ec209d33bcd2fb64fbe2c9e0f6e812186e
dkrivaa <dannykrivaa@hotmail.com>
2025-12-21 15:32:08 +0200

    Initial commit

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ pages = [home_page, check_price_page1, check_price_page2, shopping_list_page, db
 menu_pages = [home_page, check_price_page1, shopping_list_page, ]
 pg = st.navigation(pages=pages, position='hidden')
 
-nav = st.pills(
+st.pills(
     label='Menu',
     label_visibility='hidden',
     options=menu_pages,
