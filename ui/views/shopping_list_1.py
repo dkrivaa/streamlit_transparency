cd48a8bf0486327817540bf8b20297e9447f3b80
dkrivaa <dannykrivaa@hotmail.com>
2025-12-22 21:32:51 +0200

    Initial commit

--- ui/views/shopping_list_1.py
+++ ui/views/shopping_list_1.py
@@ -43,9 +43,7 @@ def render():
                     )
 
                     if store:
-                        st.session_state[chain_key] = chain
                         st.session_state[f'alias{chain_num}'] = chain.alias
-                        st.session_state[store_key] = store
 
 
 if __name__ == "__main__":
