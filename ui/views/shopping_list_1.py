7321d9852bf2f77acd32e869e6abdd31c915b466
dkrivaa <dannykrivaa@hotmail.com>
2025-12-22 21:20:10 +0200

    Initial commit

--- ui/views/shopping_list_1.py
+++ ui/views/shopping_list_1.py
@@ -1,42 +1,48 @@
 import streamlit as st
 
 from backend.utilities.general import run_async
-from backend.utilities.url_to_dict import data_dict
 from backend.data.super_class import SupermarketChain
+from backend.database.utilities import get_stores_for_chain
 
 
 def render():
     """ The main function to render the shopping list page 1 """
     with st.container():
         st.title('Make Shopping List')
-        st.subheader(' Choose up to 3 supermarket chains')
+        st.subheader('Choose up to 3 supermarket chains')
 
-        with st.container(border=True):
-            st.subheader('Supermarket Chain 1:')
-            chain1 = st.selectbox(
-                label="Select Supermarket Chain 1",
-                label_visibility='hidden',
-                options=sorted(SupermarketChain.registry, key=lambda x: x.alias),
-                format_func=lambda x: x.alias.capitalize(),
-                index=None,
-                key='chain1',
-                placeholder="Select Supermarket Chain",
-            )
-        chain2 = st.selectbox(
-            label="Select Supermarket Chain 2",
-            label_visibility='hidden',
-            options=sorted(SupermarketChain.registry, key=lambda x: x.alias),
-            format_func=lambda x: x.alias.capitalize(),
-            index=None,
-            key='chain2',
-            placeholder="Select Supermarket Chain",
-        )
-        chain3 = st.selectbox(
-            label="Select Supermarket Chain 3",
-            label_visibility='hidden',
-            options=sorted(SupermarketChain.registry, key=lambda x: x.alias),
-            format_func=lambda x: x.alias.capitalize(),
-            index=None,
-            key='chain3',
-            placeholder="Select Supermarket Chain",
-        )
+        for chain_num in range(1, 4):  # 1, 2, 3
+            chain_key = f'chain{chain_num}'
+            store_key = f'store{chain_num}'
+
+            with st.container(border=True):
+                st.subheader(f'Supermarket Chain {chain_num}:')
+
+                chain = st.selectbox(
+                    label=f"Select Supermarket Chain {chain_num}",
+                    label_visibility='hidden',
+                    options=sorted(SupermarketChain.registry, key=lambda x: x.alias),
+                    format_func=lambda x: x.alias.capitalize(),
+                    index=None,
+                    key=chain_key,
+                    placeholder="Select Supermarket Chain",
+                )
+
+                if chain:
+                    store_objects = run_async(get_stores_for_chain, chain=chain)
+                    store_options = {store.store_code: store.store_name for store in store_objects}
+
+                    store = st.selectbox(
+                        label="Select Store",
+                        label_visibility='hidden',
+                        options=sorted(list(store_options.keys()), key=int),
+                        format_func=lambda x: f'{x} - {store_options[x]}',
+                        index=None,
+                        key=store_key,
+                        placeholder="Select Supermarket Store",
+                    )
+
+                    if store:
+                        st.session_state[chain_key] = chain
+                        st.session_state[f'alias{chain_num}'] = chain.alias
+                        st.session_state[store_key] = store
