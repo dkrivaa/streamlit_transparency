5fe72e63cfbf4b2f5e307029d5abcee2f0478266
dkrivaa <dannykrivaa@hotmail.com>
2025-12-23 21:00:57 +0200

    Initial commit

--- ui/views/plan_shopping_3.py
+++ ui/views/plan_shopping_3.py
@@ -6,6 +6,37 @@ from backend.data.super_class import SupermarketChain
 from backend.database.utilities import get_stores_for_chain
 
 
+def data():
+    """ Fetches data for shopping list from the selected stores """
+    chains = [chain for chain in list(st.session_state.keys()) if chain.startswith('chain')]
+    # Dict to hold items data per chain
+    items = {}
+    # Fetch data for each selected store
+    for num in range(len(chains)):
+        chain_key = f'chain{num + 1}'
+        alias_key = f'alias{num + 1}'
+        store_key = f'store{num + 1}'
+
+        my_chain = st.session_state.get(chain_key, None)
+        alias = st.session_state.get(alias_key, None)
+        my_store = st.session_state.get(store_key, None)
+
+        try:
+            # Get fresh price and promo data
+            price_data = fresh_price_data(alias=alias, store_code=my_store) if alias and my_store else None
+            # promo_data = fresh_promo_data(alias=alias, store_code=my_store) if alias and my_store else None
+        except TypeError as e:
+            # Catch if data is getting updated on servers
+            price_data = None
+            # promo_data = None
+
+        data = [d for d in price_data if
+                d['ItemCode'] in st.session_state.get('shoppinglist', [])] if price_data else []
+        items[f'{chain_key}-{store_key}'] = data
+
+    return items
+
+
 def render():
     """ The main function to render the shopping planning page 3 """
 
@@ -16,9 +47,11 @@ def render():
 
         st.write(st.session_state)
 
-        chains = [chain for chain in list(st.session_state.keys()) if chain.startswith('chain')]
+        items_data = data()
 
-        st.write(chains)
+        st.write(items_data)
+
+
 
 
 if __name__ == "__main__":
