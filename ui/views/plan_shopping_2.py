d1be3558dc4e6955ff455d9f6697287cfe57f106
dkrivaa <dannykrivaa@hotmail.com>
2025-12-23 17:16:18 +0200

    Initial commit

--- ui/views/plan_shopping_2.py
+++ ui/views/plan_shopping_2.py
@@ -16,22 +16,40 @@ def render():
 
         st.write(st.session_state)
 
-        #
+        # Make session_state entry to hold shopping list barcodes
+        st.session_state.shoppinglist = []
+
+        # Fetch fresh data for the first selected chain and store
         my_chain = st.session_state.get('chain1', None)
         alias = my_chain.alias if my_chain else None
-        my_store = st.session_state.get('store', None)
-        # Fetch fresh data for the selected chain and store
+        my_store = st.session_state.get('store1', None)
+
         try:
             # Get fresh price and promo data
-            price_data = fresh_price_data(alias=alias, store_code=my_store) if alias and my_store else None
-            promo_data = fresh_promo_data(alias=alias, store_code=my_store) if alias and my_store else None
+            price_data1 = fresh_price_data(alias=alias, store_code=my_store) if alias and my_store else None
+            promo_data1 = fresh_promo_data(alias=alias, store_code=my_store) if alias and my_store else None
         except TypeError as e:
             # Catch if data is getting updated on servers
-            price_data = None
-            promo_data = None
+            price_data1 = None
+            promo_data1 = None
             st.error('The selected supermarket chain is currently updating the data. '
                      'Please try again in a few minutes.')
 
+        item = st.selectbox(
+                    label='Select Barcode',
+                    label_visibility='hidden',
+                    options=sorted([d['ItemCode'] for d in price_data1], key=int),
+                    format_func=lambda x: (
+                        f"{x} - "
+                        f"{next(
+                            d.get('ItemName') or d.get('ItemNm')
+                            for d in price_data1
+                            if d.get('ItemCode') == x
+                        )}"
+                    ),
+                    index=None,
+                    placeholder="Select Product Barcode",
+                )
 
 
 if __name__ == "__main__":
