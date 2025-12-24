b7aa79be7c59be963caee063d69253f5c86dc3f5
dkrivaa <dannykrivaa@hotmail.com>
2025-12-24 11:34:51 +0200

    Initial commit

--- ui/views/plan_shopping_2.py
+++ ui/views/plan_shopping_2.py
@@ -31,14 +31,14 @@ def shoppinglist_element(price_data: dict):
         )
 
         quantity = st.number_input(
-                        label='Quantity (units, kg, etc.)',
-                        label_visibility='hidden',
-                        min_value=0,
-                        value=1.0,
-                        step=1,
-                        format="%0.1f",
-                        placeholder='Enter Quantity (units, kg, etc.)'
-                    )
+            label='Quantity (units, kg, etc.)',
+            label_visibility='hidden',
+            min_value=0,
+            value=1.0,
+            step=1,
+            format="%0.1f",
+            placeholder='Enter Quantity (units, kg, etc.)'
+        )
 
         submitted = st.form_submit_button('Add to Shopping List')
         if submitted and item:
